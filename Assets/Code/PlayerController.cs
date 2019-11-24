eea01b25654a94bb9b81daf3c791107010604687
Chris Johnson <crjjrc@gmail.com>
2019-11-24 15:21:08 -0600

    Add spawn crates in line of sight.

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  public GameObject cratePrefab;
+
+  void Start() {
+
+  }
+
+  void Update() {
+    if (Input.GetButtonUp("Fire1")) {
+      GameObject instance = Instantiate(cratePrefab);
+      instance.transform.position = transform.position + transform.forward * 5 + Vector3.down * 0.5f;
+    }
+  }
+}
