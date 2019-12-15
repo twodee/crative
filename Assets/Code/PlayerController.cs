42373948888ca176d1c2e7289bffa3a04ea1ed80
Chris Johnson <crjjrc@gmail.com>
2019-12-14 19:20:25 -0600

    Anchor crates on other crates.

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -17,66 +17,61 @@ public class PlayerController : MonoBehaviour {
   }
 
   void Update() {
-    bool isCratable = false;
+    bool isGrid = false;
 
     RaycastHit hit;
     if (Physics.Raycast(transform.position, camera.forward, out hit, 5f, buildable)) {
-      GameObject target = hit.transform.gameObject;
+      GameObject target = null;
 
       Vector3Int q = Vector3Int.zero;
 
-      if (target.CompareTag("Ground")) {
+      if (hit.transform.CompareTag("Ground")) {
         Vector3 p = hit.point;
         q = new Vector3Int(Mathf.RoundToInt(p.x), Mathf.RoundToInt(p.y), Mathf.RoundToInt(p.z));
-        isCratable = IsCratable(q);
+        isGrid = IsGrid(q);
       } else {
+        target = hit.transform.gameObject;
+
         Vector3 diff = hit.point - hit.transform.position;
-        Debug.LogFormat("{0} {1} {2}", diff.x, diff.y, diff.z);
-        //print(hit.point - hit.transform.position);
+        Vector3 mag = new Vector3(Mathf.Abs(diff.x), Mathf.Abs(diff.y), Mathf.Abs(diff.z));
+        Vector3 p = hit.transform.position;
+        if (mag.x > mag.y && mag.x > mag.z) {
+          q = new Vector3Int(Mathf.RoundToInt(p.x + Mathf.Sign(diff.x)), Mathf.RoundToInt(p.y), Mathf.RoundToInt(p.z));
+        } else if (mag.y > mag.x && mag.y > mag.z) {
+          q = new Vector3Int(Mathf.RoundToInt(p.x), Mathf.RoundToInt(p.y + Mathf.Sign(diff.y)), Mathf.RoundToInt(p.z));
+        } else {
+          q = new Vector3Int(Mathf.RoundToInt(p.x), Mathf.RoundToInt(p.y), Mathf.RoundToInt(p.z + Mathf.Sign(diff.z)));
+        }
+        isGrid = IsGrid(q);
       }
 
-      if (isCratable) {
-        previewCrate.transform.position = q;
-
-        if (Input.GetButtonUp("Fire1")) {
-          GameObject instance = Instantiate(cratePrefab, prefabParent.transform);
-          instance.transform.position = q;
-          grid[q.x, q.y, q.z] = instance;
+      if (isGrid) {
+        if (target != null && Input.GetButtonUp("Fire2")) {
+          q = new Vector3Int(Mathf.RoundToInt(target.transform.position.x), Mathf.RoundToInt(target.transform.position.y), Mathf.RoundToInt(target.transform.position.z));
+          grid[q.x, q.y, q.z] = null;
+          Destroy(target);
+        } else {
+          previewCrate.transform.position = q;
+          if (Input.GetButtonUp("Fire1")) {
+            GameObject instance = Instantiate(cratePrefab, prefabParent.transform);
+            instance.transform.position = q;
+            grid[q.x, q.y, q.z] = instance;
+          }
         }
       }
     }
 
-    previewCrate.SetActive(isCratable);
-
-    //  {
-    //  Vector3 p = transform.position + transform.forward * 5 + Vector3.down * 0.5f;
-    //  Vector3Int q = new Vector3Int(Mathf.RoundToInt(p.x), Mathf.RoundToInt(p.y), Mathf.RoundToInt(p.z));
-    //  bool isCratable = IsCratable(q);
-    //  previewCrate.SetActive(isCratable);
-    //  if (isCratable) {
-    //    previewCrate.transform.position = q;
-    //  }
-
-    // 
-    //}
-
-    //if (Input.GetButtonUp("Fire2")) {
-    //  RaycastHit hit;
-    //  if (Physics.Raycast(transform.position, transform.forward, out hit, 5, 1 << 8)) {
-    //    Vector3 p = hit.transform.position;
-    //    Vector3Int q = new Vector3Int(Mathf.RoundToInt(p.x), Mathf.RoundToInt(p.y), Mathf.RoundToInt(p.z));
-    //    grid[q.x, q.y, q.z] = null;
-    //    print(q);
-    //    Destroy(hit.transform.gameObject);
-    //  }
-    //}
+    previewCrate.SetActive(isGrid);
   }
 
-  private bool IsCratable(Vector3Int p) {
+  private bool IsCrate(Vector3Int p) {
+    return grid[p.x, p.y, p.z] != null;
+  }
+
+  private bool IsGrid(Vector3Int p) {
     return p.x >= 0 && p.x <= 20 &&
       p.y >= 0 && p.y <= 10 &&
-      p.z >= 0 && p.z <= 20 &&
-      grid[p.x, p.y, p.z] == null;
+      p.z >= 0 && p.z <= 20;
   }
 }
 
